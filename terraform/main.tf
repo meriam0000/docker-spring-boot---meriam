f4c241bd633257a3c56c0c2ac5845e7749768dbf
ThourayaLouati <thouraya.louati@esprit.tn>
2024-10-22 12:35:19 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "aws_eks_cluster" "my_cluster" {
 
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "my-node-group"
+  node_group_name = "noeud1"
   node_role_arn   = "arn:aws:iam::744983671605:role/LabRole"
   subnet_ids      = ["subnet-075e0dd45da90bdb8", "subnet-0114974e6dbd8217e"]
 
