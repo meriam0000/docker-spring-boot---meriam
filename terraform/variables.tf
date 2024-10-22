6abb291184bc53bd7546d07edb1a45e73e7a62a2
ThourayaLouati <thouraya.louati@esprit.tn>
2024-10-22 07:20:23 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,19 @@
 variable "aws_region" {
   description = "La région AWS"
-  default     = "us-west-2"
+  default     = "us-east-1"  # Région mise à jour
 }
 
 variable "cluster_name" {
   description = "Nom du cluster EKS"
-  default     = "my-cluster"
+  default     = "mykubernetes"  # Nom du cluster mis à jour
 }
 
 variable "subnet_ids" {
   description = "IDs des sous-réseaux"
   type        = list(string)
+}
+
+variable "role_arn" {
+  description = "ARN du rôle IAM pour EKS"
+  type        = string
 }
