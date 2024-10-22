64bfbc1cf35ec423f931564cb0349ca5f71a5b70
ThourayaLouati <thouraya.louati@esprit.tn>
2024-10-22 12:17:01 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_eks_cluster" "my_cluster" {
   role_arn = "arn:aws:iam::744983671605:role/LabRole"  # Utilise l'ARN de rôle fourni
 
   vpc_config {
-    subnet_ids = ["subnet-12345678", "subnet-87654321"]  # Remplace par tes IDs de sous-réseaux
+    subnet_ids = ["subnet-075e0dd45da90bdb8", "subnet-0114974e6dbd8217e"]  # Remplace par tes IDs de sous-réseaux
   }
 }
 
