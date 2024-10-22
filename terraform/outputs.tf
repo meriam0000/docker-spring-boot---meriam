6e3db0d136118f6bb9c57bd31f574b4a01837244
ThourayaLouati <thouraya.louati@esprit.tn>
2024-10-22 12:39:07 +0000

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "cluster_endpoint" {
   description = "L'endpoint du cluster EKS"
-  value       = aws_eks_cluster.mykubernetes.endpoint  # Mise à jour du nom du cluster
+  value       = aws_eks_cluster.my_cluster.endpoint  # Utilisation du nom correct du cluster
 }
 
 output "cluster_name" {
   description = "Le nom du cluster EKS"
-  value       = aws_eks_cluster.mykubernetes.name  # Mise à jour du nom du cluster
+  value       = aws_eks_cluster.my_cluster.name  # Utilisation du nom correct du cluster
 }
 
 output "cluster_role_arn" {
   description = "L'ARN du rôle IAM du cluster EKS"
-  value       = aws_eks_cluster.mykubernetes.role_arn
+  value       = aws_eks_cluster.my_cluster.role_arn  # Utilisation du nom correct du cluster
 }
