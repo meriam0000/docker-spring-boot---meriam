7ee37ec9430a46f95b9639a3a0d8a8f015d37fd5
ThourayaLouati <thouraya.louati@esprit.tn>
2024-10-22 12:31:05 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,11 @@ provider "aws" {
 resource "aws_eks_cluster" "my_cluster" {
   name     = "mykubernetes"
   role_arn = "arn:aws:iam::744983671605:role/LabRole"
+  version  = "1.21"  # Ajoutez la version de Kubernetes
 
   vpc_config {
     subnet_ids = ["subnet-075e0dd45da90bdb8", "subnet-0114974e6dbd8217e"]
+    security_group_ids = ["sg-0123456789abcdef0"]  # Remplacez par le groupe de sécurité
   }
 }
 
@@ -22,4 +24,12 @@ resource "aws_eks_node_group" "my_node_group" {
     max_size     = 3
     min_size     = 1
   }
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.my_cluster.endpoint
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.my_cluster.name
 }
