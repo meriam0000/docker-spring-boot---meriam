2cc89a66eb11a19254e8f07a689568bc549079ab
ThourayaLouati <thouraya.louati@esprit.tn>
2024-10-23 19:40:29 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,44 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
-variable "aws_region" {
-  description = "La région AWS"
-  type        = string
-  default     = "us-east-1"  # Région mise à jour
-}
-
-variable "cluster_name" {
-  description = "Nom du cluster EKS"
-  type        = string
-  default     = "mykubernetes"  # Nom du cluster mis à jour
-}
-
-variable "subnet_ids" {
-  description = "IDs des sous-réseaux"
-  type        = list(string)
-  default     = ["subnet-075e0dd45da90bdb8", "subnet-0114974e6dbd8217e"]  # Valeurs par défaut
-}
-
-variable "role_arn" {
-  description = "ARN du rôle IAM pour EKS"
-  type        = string
-  default     = "arn:aws:iam::744983671605:role/LabRole"  # Valeur par défaut
-}
-
-variable "vpc_id" {
-  description = "L'ID du VPC pour le cluster EKS"
-  type        = string
-  default     = "vpc-04c984733bc5b455e"  # Remplacez par votre ID de VPC réel
-}
-
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"  # Assurez-vous que cela correspond à vos besoins
+  cidr_block = var.vpc_cidr  # Utilisation de la variable pour le CIDR
 }
 
 resource "aws_security_group" "eks_cluster_sg" {
   name        = "eks-cluster-sg-${var.cluster_name}"
   description = "Security group for EKS cluster ${var.cluster_name}"
-  vpc_id      = var.vpc_id  # Utilisez l'ID de VPC défini dans les variables
+  vpc_id      = var.vpc_id  # Utilisation de la variable pour l'ID du VPC
 
   ingress {
     from_port   = 8083
@@ -70,7 +40,7 @@ resource "aws_security_group" "eks_cluster_sg" {
 resource "aws_security_group" "eks_worker_sg" {
   name        = "eks-worker-sg-${var.cluster_name}"
   description = "Security group for EKS worker nodes ${var.cluster_name}"
-  vpc_id      = var.vpc_id  # Utilisez l'ID de VPC défini dans les variables
+  vpc_id      = var.vpc_id  # Utilisation de la variable pour l'ID du VPC
 
   ingress {
     from_port   = 8083
@@ -120,6 +90,4 @@ resource "aws_eks_node_group" "my_node_group" {
     max_size     = 3
     min_size     = 1
   }
-
-  # Les groupes de sécurité sont gérés par la configuration EKS
 }
